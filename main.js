1cc7f2997394fab90460de198d880dc7f58f8530
Rita Ślązak <ritabobrowska@gmail.com>
2020-05-14 21:50:17 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('siema!');
+const name = 'Rita';
+const age = 28;
+console.log(name);
+console.log(age);
+console.log(`Nazywam się ${name} i mam ${age} lat.`);
+
+console.log(`Witaj przybyszu`)
